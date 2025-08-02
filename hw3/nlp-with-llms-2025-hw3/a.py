a8a9e19b67ed99c0a2888a2e62003b73a8a08ca2
hadar88 <hadargantz@gmail.com>
2025-08-02 21:03:13 +0300

    changes

---
+++ hw3/nlp-with-llms-2025-hw3/a.py
@@ -0,0 +1,42 @@
+import os
+import json
+
+
+# make a list of all the folders in the folder ../PragmatiCQA-sources
+def list_folders(directory="../PragmatiCQA-sources"):
+    return [d for d in os.listdir(directory) if os.path.isdir(os.path.join(directory, d))]
+folders = list_folders()
+
+topics = set()
+
+with open("../PragmatiCQA/data/test.jsonl", 'r') as file:
+    for line in file:
+        data = json.loads(line)
+        if 'topic' in data:
+            topics.add(data['topic'])
+
+with open("../PragmatiCQA/data/val.jsonl", 'r') as file:
+    for line in file:
+        data = json.loads(line)
+        if 'topic' in data:
+            topics.add(data['topic'])
+
+
+# print("Folders in ../PragmatiCQA-sources:")
+# print(folders)
+
+# print("\nTopics in ../PragmatiCQA/data/test.jsonl:")
+# print(topics)
+
+for topic in topics:
+    if topic not in folders:
+        print(topic)
+
+# Po
+# The Wonderful Wizard of Oz (book)
+# Spirited Away
+# A Nightmare on Elm Street (2010 film)
+# New York Yankees
+# Snoopy
+# Alexander Hamilton
+# Popeye
