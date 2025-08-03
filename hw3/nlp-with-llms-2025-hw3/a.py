d3f9ed6f7f4c8357a008c97a73755a632dc07b39
hadar88 <hadargantz@gmail.com>
2025-08-03 09:26:17 +0300

    changes

--- hw3/nlp-with-llms-2025-hw3/a.py
+++ hw3/nlp-with-llms-2025-hw3/a.py
@@ -1,25 +1,40 @@
 import os
 import json
+import hashlib
+
+def get_cache_key(topic, question):
+        key_str = f"{topic}|{question}"
+        return hashlib.md5(key_str.encode()).hexdigest()
 
 
 # make a list of all the folders in the folder ../PragmatiCQA-sources
-def list_folders(directory="../PragmatiCQA-sources"):
-    return [d for d in os.listdir(directory) if os.path.isdir(os.path.join(directory, d))]
-folders = list_folders()
+# def list_folders(directory="../PragmatiCQA-sources"):
+#     return [d for d in os.listdir(directory) if os.path.isdir(os.path.join(directory, d))]
+# folders = list_folders()
 
-topics = set()
 
-# with open("../PragmatiCQA/data/test.jsonl", 'r') as file:
+
+s = set()
+
+with open("../PragmatiCQA/data/test.jsonl", 'r') as file:
+    for line in file:
+        data = json.loads(line)
+        topic = data.get('topic')
+        if topic in ["Snoopy"]:
+            continue
+        qas = data.get('qas')[0]
+        question = qas['q']
+        hash = get_cache_key(topic, question)
+        if hash in s:
+             print(f"Duplicate found: {hash} for topic: {topic} and question: {question}")
+        else:
+            s.add(hash)
+
+# with open("../PragmatiCQA/data/val.jsonl", 'r') as file:
 #     for line in file:
 #         data = json.loads(line)
 #         if 'topic' in data:
-#             topics.add(data['topic'])
-
-with open("../PragmatiCQA/data/val.jsonl", 'r') as file:
-    for line in file:
-        data = json.loads(line)
-        if 'topic' in data:
-            topics.add(data['topic'])
+#             topics.append(data['topic'])
 
 
 # print("Folders in ../PragmatiCQA-sources:")
@@ -27,10 +42,12 @@ with open("../PragmatiCQA/data/val.jsonl", 'r') as file:
 
 # print("\nTopics in ../PragmatiCQA/data/test.jsonl:")
 # print(topics)
+# count = 0
+# for topic in topics:
+#     if topic not in folders:
+#         count += 1
 
-for topic in topics:
-    if topic not in folders:
-        print(topic)
+# print(count)
 
 # A Nightmare on Elm Street (2010 film)
 # Popeye
